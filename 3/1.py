d3c1fe70f625dea6597e07ef725b2bf89df19168
Maxim Valyarenko <mvalyarenko@gmail.com>
2024-03-13 11:57:02 +0300

    update 5.py

--- 3/1.py
+++ 3/1.py
@@ -24,7 +24,7 @@ def hash(s):
 with open('students (1).csv', encoding='utf-8') as file, open('students_with_hash.csv', 'w', encoding='utf-8') as new_file:
     data = list(csv.reader(file, delimiter=','))
     res = csv.writer(new_file, delimiter=',')
-
+    res.writerow(data[0])
     for stroka in data[1:]:
         fio = stroka[1]
         h = hash(fio)
